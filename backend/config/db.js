37178d66f26dc66888e66ec65c3e309c8a2fbd07
rishwanth0 <srishwanth02@gmail.com>
2025-07-04 01:00:40 +0530

    Apply clean .gitignore for node_modules and .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://srishwanth02:LtJlvvHRJec5EfkJ@cluster0.wv67hbs.mongodb.net/food-delivery')
-    .then(()=>console.log("DB connected"));
+    await mongoose.connect(process.env.MONGO_URL)
+    .then(()=>console.log("DB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 }
