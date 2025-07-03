9512398cfcd00659920d4824d2929253781c55b8
rishwanth0 <srishwanth02@gmail.com>
2025-07-04 02:20:52 +0530

    frontend update

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext  = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = import.meta.env.VITE_API_URL;
+    const url = "https://your-backend.onrender.com";
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([]);
 
